d9befdcbce9bea008052ebfc41895315d1a00d04
divine-1111 <bt21ece025@nituk.ac.in>
2025-04-10 15:58:44 +0530

    1st phase setup done

--- brain_doctor.py
+++ brain_doctor.py
@@ -1,12 +1,36 @@
 import os
 
-groq_api_key=os.environ.get("groq_api_key")
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
 
 import base64
 image_path="acne.jpg"
 image_file=open(image_path,"rb")
-encoded_imae=base64.b64encode(image_file.read()).decode('utf-8')
+encoded_image=base64.b64encode(image_file.read()).decode('utf-8')
 
-from groq import groq
+from groq import Groq
 
 client=Groq()
+query="What is wrong with my face?"
+model="llama-3.2-90b-vision-preview"
+messages=[
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text", 
+                "text": query
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/jpeg;base64,{encoded_image}",
+                },
+            },
+        ],
+    }]
+
+chat_completion=client.chat.completions.create(
+    messages=messages,
+    model=model
+)
+print(chat_completion.choices[0].message.content)
